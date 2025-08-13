c79de494065950b83a8ef7e9dfa804868dcc42ef
Matheus Valandro <msvalandro@proton.me>
2025-08-13 21:22:53 +0100

    Section 16: Project - RDS Module

---
+++ proj04-rds-module/rds.tf
@@ -0,0 +1,15 @@
+module "database" {
+  source = "./modules/rds"
+
+  project_name = "proj04-rds-module"
+  security_group_ids = [
+    aws_security_group.compliant.id
+  ]
+  subnet_ids = [
+    aws_subnet.private1.id,
+  ]
+  credentials = {
+    username = "dbadmin"
+    password = "abc1+?_ahu"
+  }
+}
